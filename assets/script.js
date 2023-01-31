7805c8fbe87087f9f6c4c94e69348c3ccd13408a
markmos1991 <101826190+markmos1991@users.noreply.github.com>
2023-01-31 17:27:54 +0000

    Javacript pulls data form API

--- assets/script.js
+++ assets/script.js
@@ -1,8 +1,63 @@
 
  // APi Key
 var APIKey = "f33f184931595a6e106cb189ecdd0a1b";
-let lat = 0;
-let lon = 0;
+
+function getLocation(cityName){
+  var queryURL = "http://api.openweathermap.org/geo/1.0/direct?q=" + cityName + "&limit=1&appid=" + APIKey;  
+  // returns response based on city name - lat lon
+      $.ajax({
+        url: queryURL,
+        method: "GET"
+      }).then(function(response) {
+    
+        let lat = response[0].lat.toFixed(2);
+        let lon = response[0].lon.toFixed(2);
+
+        getWeather(lat,lon);
+
+        // sae name from here to local storage
+      
+
+})
+}
+
+
+function getWeather(lat, lon){
+    //   second query for lat lon
+    var queryURL2 = "http://api.openweathermap.org/data/2.5/forecast?lat=" + lat + "&lon=" + lon + "&units=metric&appid=" + APIKey ;
+
+    $.ajax({
+      url: queryURL2,
+      method: "GET"
+    }).then(function(response) {
+
+      console.log(response);
+
+      renderWeather(response)
+
+
+
+});
+  
+  
+}
+
+function renderWeather(weatherData){
+  var currentDay = weatherData.list[0];
+  
+
+  $("#city").text(weatherData.city.name);
+  $("#wind").text("Wind Speed: " + currentDay.wind.speed + " MPH");
+  $("#humidity").text("Humidity: " + currentDay.main.humidity + "%");
+  $("#temp").text("Temperature: " + currentDay.main.temp);
+
+  $("#renderSection").html("");
+  // create a container, then contents of the container, add the contents inside the container, put the container on the page
+  for (var i = 8; i < weatherData.list.length; i++) {
+
+}
+
+
 
  // This .on("click") function will trigger the AJAX Call
  $("#find-weather").on("click", function(event) {
@@ -10,39 +65,21 @@ let lon = 0;
     event.preventDefault();
     // City name taken from input
     var cityName = $("#city-input").val();
+    getLocation(cityName);
 
 // Here we are building the URL we need to query the database based on city name
 
-
-   var queryURL = "http://api.openweathermap.org/geo/1.0/direct?q=" + cityName + "&limit=1&appid=" + APIKey;  
-// returns response based on city name - lat lon
-    $.ajax({
-      url: queryURL,
-      method: "GET"
-    }).then(function(response) {
-  
-      let lat = response[0].lat.toFixed(2);
-      let lon = response[0].lon.toFixed(2);
-      console.log(response);
-      console.log(response[0].lat);
-      console.log(response[0].lon);
-      console.log(lat);
-      console.log(lon);
-    
+// get location 
+});
 
 
-    //   second query for lat lon
-      var queryURL2 = "https://api.openweathermap.org/data/2.5/forecast?lat=" + lat; "&lon=" + lon; "&appid=9ee38f46bd8220a330649af11be415b2";
 
-      $.ajax({
-        url: queryURL2,
-        method: "GET"
-      }).then(function(response) {
+// dt from object for time
+var newDate = new Date(1675123200 *1000)
 
-        console.log(response);
-    
-  
-  });
-    
+console.log(newDate)
 
- })});
+// local storage to save searched cities 
+
+// append into cards
+
